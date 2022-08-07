ad7a161c8267567165390497451ce93689d178b5
Daniel Machado Vasconcelos <danielm.vasconcelos@gmail.com>
2022-08-07 20:45:27 +0200

    HM-10 Added the vertx framework to handle http request

--- src/main/java/br/com/ead/home/Application.java
+++ src/main/java/br/com/ead/home/Application.java
@@ -7,17 +7,6 @@ import lombok.extern.log4j.Log4j2;
 public class Application {
 
     public static void main(String[] args) {
-        InitialContext initialContext = InMemoryInitialContext.getInstance();
-        Environment environment = new SystemEnvironmentVariables();
-        NamespaceResolver resolver = new InMemoryNamespaceResolver(environment);
-        InMemoryBeanLoader loader = new InMemoryBeanLoader(environment, initialContext, resolver);
-        loader.init();
-
-        TimeSlot timeSlot = Slot.from(today, Duration.ofHours(1));
-
-        Appointment appointment = controller.createAppointment(clinicianId, patientId, timeSlot);
-        log.info("New Appointment created: {}", appointment);
-
-        return appointment;
+        Launcher.main(new String[]{"run", "br.com.ead.home.MainVerticle"});
     }
 }
